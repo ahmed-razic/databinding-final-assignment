c12c43ebfe527de90ac3fea55f1633d2bd3d70c4
ahmed.razic <ahmed.razic@gmail.com>
2022-10-29 16:15:00 +0200

    finished assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [
+    `
+      .white {
+        color: white;
+      }
+    `,
+  ],
 })
 export class AppComponent {
+  secretPassword: string = 'Secret Password = tuna';
+  displayOn: boolean = false;
+  items: any = [];
   title = 'databinding-final-assignment';
+
+  changeDisplay() {
+    this.displayOn = !this.displayOn;
+    this.items.push(this.secretPassword);
+  }
 }
